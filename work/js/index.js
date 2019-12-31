11a8f74dfbde61eaf7cdf02afed97398a1436034
Kim Han Ho <fj2746@gmail.com>
2019-12-31 12:57:41 +0900

    sample

--- work/js/index.js
+++ work/js/index.js
@@ -1,10 +1,11 @@
 
-class mySwiper {
+class SwiperClass {
     constructor(){
-        new Swiper ('.swiper-container', {
+
+        let param = {
             // Optional parameters
             // direction: 'vertical',
-            loop: true,
+            loop: false,
 
             // If we need pagination
             pagination: {
@@ -20,11 +21,44 @@ class mySwiper {
             // And if we need scrollbar
             scrollbar: {
                 el: '.swiper-scrollbar',
-            },
-        });
+            }
+
+            , autoHeight:true
+            // Tranisition effect. Could be "slide", "fade", "cube", "coverflow" or "flip"
+            // , effect: 'cube'
+
+            // slide 시작 위치
+            , initialSlide: 2
+
+            // 슬라이드 간격
+            , spaceBetween:500
+        };
+
+        this.mySwiper = new Swiper('.swiper-container', param);
+        this.realIndex();
+        this.slideChangeTransition();
     }
+
+
+    realIndex(){
+        console.log(this.mySwiper.realIndex, '> this.realIndex');
+    }
+
+    slideChangeTransition(){
+
+        this.mySwiper.on('slideChangeTransitionStart', ()=>{
+            console.log('slideChangeTransitionStart');
+            const idx = this.mySwiper.activeIndex;
+            console.log($(this.mySwiper.slides[idx]), '> $(this.mySwiper.slides[idx])', idx, '> idx');
+        });
+
+        this.mySwiper.on('slideChangeTransitionEnd', function () {
+            console.log('slideChangeTransitionEnd');
+        }.bind(this));
+    }
+
 }
 
 $(()=>{
-    new mySwiper();
+    new SwiperClass();
 });
